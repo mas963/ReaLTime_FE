96ad3904d836d5e7714f402317e95dde8904f861
Yasar Can <38841941+mas963@users.noreply.github.com>
2025-03-20 22:48:51 +0300

    edit: dashboard logo

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,6 @@
 import { Header } from "@/components/dashboard/header"
+import { NextAuthProvider } from "@/providers/NextAuthProvider"
+import { ThemeProvider } from "@/providers/theme-provider"
 import { Toaster } from "sonner"
 
 export default function DashboardLayout({
@@ -8,11 +10,19 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="flex min-h-screen flex-col">
-      <Header />
-      <Toaster />
-      <main className="mx-auto w-full max-w-7xl flex-1 space-y-4 p-8 pt-6">
-        {children}
-      </main>
+      <NextAuthProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange>
+          <Header />
+          <Toaster />
+          <main className="mx-auto w-full max-w-7xl flex-1 space-y-4 p-8 pt-6">
+            {children}
+          </main>
+        </ThemeProvider>
+      </NextAuthProvider >
     </div>
   )
 }
--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -5,16 +5,26 @@ import { ModeToggle } from "@/components/mode-toggle"
 import { UserNav } from "@/components/dashboard/user-nav"
 import Image from "next/image"
 import Link from "next/link"
-import { useTheme } from "next-themes";
 
 export function Header() {
-  const { theme } = useTheme();
-
   return (
     <div className="border-b">
       <div className="flex h-16 items-center lg:px-16 px-2">
         <Link href="/dashboard">
-          <Image src={theme === "dark" ? "/wolfsprintWhiteLogo.png" : "/wolfsprintLogo.png"} alt="logo" width={160} height={160} />
+          <Image
+            width={160}
+            height={160}
+            src="/ReaLTimeLogo.png"
+            alt="logo"
+            className="block dark:hidden"
+          />
+          <Image
+            width={160}
+            height={160}
+            src="/ReaLTimeLogo-White.png"
+            alt="logo"
+            className="hidden dark:block"
+          />
         </Link>
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
